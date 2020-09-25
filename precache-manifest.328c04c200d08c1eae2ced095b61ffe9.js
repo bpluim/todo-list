20698f8a02793c97b360493a40388562940eee70
Ben Pluim <ben.pluim@fmgsuite.com>
2020-09-25 11:38:09 -0600

    Updates

---
+++ precache-manifest.328c04c200d08c1eae2ced095b61ffe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8af15a236ae68e5e94f55bbbfcdf34fa",
+    "url": "/todo_list/index.html"
+  },
+  {
+    "revision": "3b495f8d6a43822da90e",
+    "url": "/todo_list/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "88eec06702897b37e3bd",
+    "url": "/todo_list/static/js/2.c7655307.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo_list/static/js/2.c7655307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b495f8d6a43822da90e",
+    "url": "/todo_list/static/js/main.c5b26ee1.chunk.js"
+  },
+  {
+    "revision": "97ce3312d6d2c4f98d72",
+    "url": "/todo_list/static/js/runtime-main.9297ec14.js"
+  }
+]);
